eb16e02aa1b6b0401de72d220a11f5b742c981db
Austin Blake <ummwhere@hotmail.com>
2017-12-16 22:37:28 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('form').on('keydown', function(key) {
+  $('#typing').on('keydown', function(key) {
     if(key.which === 071) {
       alert('g was pressed');
     }
